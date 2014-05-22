944e47a349edabd437d0a2d2a9eda45c4b88e48c
Dustin Shahidehpour <dshahidehpour@gmail.com>
2014-05-22 18:47:31 -0500

    we can delete things too.

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -11,7 +11,9 @@ app.controller('PostsCtrl', function($scope, Post) {
         });
     };
 
-    $scope.deletePost = function(index) {
-        $scope.posts.splice(index, 1);
-    }
+    $scope.deletePost = function(postId) {
+        Post.delete({id: postId}, function() {
+            delete $scope.posts[postId];
+        });
+    };
 });
