fe0925ce0323a0df614c69957b68d6c1bfb566fe
Dustin Shahidehpour <dshahidehpour@gmail.com>
2014-05-23 00:02:55 -0500

    Added submissions to their page.

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -2,7 +2,9 @@
 
 app.controller('PostsCtrl', function($scope, $location, Post) {
     $scope.post = { url: 'http://', title: ''};
-    $scope.posts = Post.all;
+    if ($location.path() === '/') {
+        $scope.posts = Post.all;
+    }
 
     $scope.deletePost = function(postId) {
         Post.delete(postId);
