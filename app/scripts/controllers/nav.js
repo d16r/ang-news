68d141347d631ec2c689995d17d0be0e0ce23523
Dustin Shahidehpour <dshahidehpour@gmail.com>
2014-05-22 23:02:22 -0500

    Added the login and logout functionality

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -1,7 +1,11 @@
 'use strict';
 
-app.controller('NavCtrl', function($scope, $location, Post) {
+app.controller('NavCtrl', function($scope, $location, Post, Auth) {
     $scope.post = {url: 'http://', title: ''};
+
+    $scope.logout = function() {
+        Auth.logout();
+    }
 
     $scope.submitPost = function() {
         Post.create($scope.post).then(function(ref) {
